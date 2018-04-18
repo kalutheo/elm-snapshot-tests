8b6e290e09ab9b01095fa084561b1bbf074b0c20
Théophile KALUMBU <kalutheo@hotmail.com>
2018-04-18 17:50:22 +0200

    feat(multi): handle multiple describes

--- ExampleBmiCalculatorTest.elm
+++ ExampleBmiCalculatorTest.elm
@@ -5,10 +5,18 @@ import SnapShotTest exposing (..)
 
 
 tests =
-    describe "A Small person BMI"
+    [ describe "A small person BMI"
         ([ it "Select a Gender" (SelectGender Female)
          , it "Select a Height" (SelectHeight "150")
          , it "Select a Weight" (SelectWeight "55")
          ]
             |> runTests initialModel update
         )
+    , describe "A tall and slim person BMI"
+        ([ it "Select a Gender" (SelectGender Male)
+         , it "Select a Height" (SelectHeight "190")
+         , it "Select a Weight" (SelectWeight "55")
+         ]
+            |> runTests initialModel update
+        )
+    ]
--- template.js
+++ template.js
@@ -45,16 +45,19 @@ module.exports = ElmSnapshotsEnvironment;
 `;
 
 const getTestRunner = () => `
-const chalk = require('chalk');
+const chalk = require("chalk");
 const Elm = require("./PrivateMain.js");
 const log = console.log;
 
-describe(snapshots.test, () => {
-  snapshots.actions.forEach((snapshot) => {
-    test(snapshot.title, () => {
-      expect(snapshot).toMatchSnapshot();
-    })
-  })
+snapshots.forEach(snapshotEntry => {
+  console.log("snapshotEntry", snapshotEntry);
+  describe(snapshotEntry.test, () => {
+    snapshotEntry.actions.forEach(snapshot => {
+      test(snapshot.title, () => {
+        expect(snapshot).toMatchSnapshot();
+      });
+    });
+  });
 });
 `;
 const getElmApp = testModule => `
@@ -64,30 +67,36 @@ import ${testModule} exposing (tests)
 import Json.Encode exposing (encode, list, object, string)
 import Platform
 
+
 port outGoing : String -> Cmd msg
 
+
 makeSnapshot ( testSuiteTitle, testsResults ) =
-  let
-      tests =
-          testsResults
-              |> List.map
-                  (\\{ title, msg, newModel } ->
-                      object
-                          [ ( "title", string title )
-                          , ( "msg", string (msg |> toString) )
-                          , ( "newModel", string (newModel |> toString) )
-                          ]
-                  )
-  in
-  object [ ( "test", string testSuiteTitle ), ( "actions", list tests ) ] |> encode 4
+    let
+        tests =
+            testsResults
+                |> List.map
+                    (\\{ title, msg, newModel } ->
+                        object
+                            [ ( "title", string title )
+                            , ( "msg", string (msg |> toString) )
+                            , ( "newModel", string (newModel |> toString) )
+                            ]
+                    )
+    in
+    object [ ( "test", string testSuiteTitle ), ( "actions", list tests ) ]
+
+
+makeSnapShotList tests =
+    list (tests |> List.map makeSnapshot)
 
 
 main =
-  Platform.program
-      { init = ( (), outGoing (tests |> makeSnapshot) )
-      , update = \\() -> \\() -> ( (), Cmd.none )
-      , subscriptions = \\() -> Sub.none
-      }
+    Platform.program
+        { init = ( (), outGoing (tests |> makeSnapShotList |> encode 4) )
+        , update = \\() -> \\() -> ( (), Cmd.none )
+        , subscriptions = \\() -> Sub.none
+        }
 `;
 
 module.exports = {
