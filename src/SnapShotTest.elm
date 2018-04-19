2f628e5fe9c6089e4045efed6a8b73a7bade7870
Théophile KALUMBU <kalutheo@hotmail.com>
2018-04-19 15:17:23 +0200

    feat(docs): add documentation

--- src/SnapShotTest.elm
+++ src/SnapShotTest.elm
@@ -1,20 +1,34 @@
-module SnapShotTest exposing (..)
+module SnapShotTest exposing (describe, it, snapshotUpdate)
+
+{-| Test your Elm update function with Snapshot Testing
+
+@docs it
+@docs describe
+@docs snapshotUpdate
+
+-}
 
 
 type alias Test msg a =
     { msg : msg, newModel : a, title : String }
 
 
+{-| Group and describe a list of test
+-}
 describe : String -> b -> ( String, b )
 describe title testCases =
     ( title, testCases )
 
 
+{-| Define a test with a message
+-}
 it : String -> msg -> ( String, msg )
 it title action =
     ( title, action )
 
 
+{-| Creates a record that can be serialized and snapshoted with Jest.
+-}
 snapshotUpdate : model -> (msg -> model -> ( model, cmd )) -> List ( String, msg ) -> List (Test msg model)
 snapshotUpdate model updateCallback testCases =
     testCases
